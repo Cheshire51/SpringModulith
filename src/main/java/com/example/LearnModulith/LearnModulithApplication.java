930bc6492cbb738a1b995554c020dab0f5e1e039
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-01-25 23:08:36 +0700

    init project

---
+++ src/main/java/com/example/LearnModulith/LearnModulithApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnModulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearnModulith/LearnModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnModulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
