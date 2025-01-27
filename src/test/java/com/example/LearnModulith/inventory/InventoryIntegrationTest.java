d6cfabd836143ec9521c9d85856ecc1013ce64bd
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-01-27 22:54:13 +0700

    Simple Integration + config database connection for testing

---
+++ src/test/java/com/example/LearnModulith/inventory/InventoryIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.example.LearnModulith.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+@ApplicationModuleTest
+class InventoryIntegrationTest {
+    @Test
+    void verifyModule(){
+
+    }
+}
