e2e42d12d4af7585a8478c9d821c9207de75fb00
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-01-27 16:06:15 +0700

    Limit repo access level (by remove public keyword)

--- src/main/java/com/example/LearnModulith/inventory/InventoryRepository.java
+++ src/main/java/com/example/LearnModulith/inventory/InventoryRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface InventoryRepository extends CrudRepository<Inventory, Long> {
+interface InventoryRepository extends CrudRepository<Inventory, Long> {
     Optional<Inventory> getInventoryByName(String inventoryName);
 
     List<Inventory> getInventoriesByNameIn(List<String> inventoryNames);
