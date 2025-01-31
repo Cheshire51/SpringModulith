7b06e66ab14ab06dacae1d08a050f6481c1f3120
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-01-31 23:27:28 +0700

    Integration test for creating order

--- src/test/java/com/example/LearnModulith/order/OrderIntegrationTest.java
+++ src/test/java/com/example/LearnModulith/order/OrderIntegrationTest.java
@@ -1,13 +1,37 @@
 package com.example.LearnModulith.order;
 
+import com.example.LearnModulith.order.dto.InventoryRequestDto;
+import com.example.LearnModulith.order.dto.OrderDto;
+import com.example.LearnModulith.order.dto.OrderResponseDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ApplicationModuleTest(mode = ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
 class OrderIntegrationTest {
+
+    @Autowired
+    OrderService orderService;
+
     @Test
     void verify()
     {
 
+    }
+
+    @Test
+    void createOrder() {
+        List<InventoryRequestDto> inventoryDtoList = List.of(
+                new InventoryRequestDto("ruler", 4),
+                new InventoryRequestDto("pencil",3)
+        );
+        OrderDto orderDto = new OrderDto("Customer's name", "testtest123@gmail.com", inventoryDtoList);
+        OrderResponseDto order = orderService.createOrder(orderDto);
+        assertThat(order.message()).isEqualTo("Order Currently Processed");
+        assertThat(order.statusCode()).isEqualTo(102);
     }
 }
