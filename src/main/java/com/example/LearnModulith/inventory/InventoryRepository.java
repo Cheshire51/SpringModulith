03b69ce73f917892bbb317d67df8a33a150259ea
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-02-01 20:50:53 +0700

    Remove unnecessary annotation

--- src/main/java/com/example/LearnModulith/inventory/InventoryRepository.java
+++ src/main/java/com/example/LearnModulith/inventory/InventoryRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 interface InventoryRepository extends CrudRepository<Inventory, Long> {
     Optional<Inventory> getInventoryByName(String inventoryName);
 
