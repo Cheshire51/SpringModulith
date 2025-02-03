d3f2bb7af629b6fcde06b267d5db1874744cc9e4
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-02-03 11:19:25 +0700

    Add dependency for validation + create EmailIntegrationTest

---
+++ src/test/java/com/example/LearnModulith/email/EmailIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.example.LearnModulith.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+
+@ApplicationModuleTest
+class EmailIntegrationTest {
+    @Test
+    void verifyEmail()
+    {
+
+    }
+}
